f5f6f2902027471c405cce488d5b73eb9ea162fb
SargisKhlopuzyan1989 <skholpuzyan@shophq.com>
2021-11-08 11:50:25 +0400

    Part 1 - MVVM Running App (WITH DAGGER-HILT) - Project Overview & Architecture

---
+++ app/src/main/java/com/sargis/khlopuzyan/pl_mvvmrunningapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sargis.khlopuzyan.pl_mvvmrunningapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
