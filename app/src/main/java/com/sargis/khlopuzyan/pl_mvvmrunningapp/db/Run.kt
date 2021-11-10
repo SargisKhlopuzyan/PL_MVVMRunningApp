6f0e71b204704f208bfd87a110a46d3e481f1b21
SargisKhlopuzyan1989 <skholpuzyan@shophq.com>
2021-11-10 17:54:10 +0400

    Running Database & TypeConverter - MVVM Running Tracker App (WITH DAGGER-HILT!) - Part 4

--- app/src/main/java/com/sargis/khlopuzyan/pl_mvvmrunningapp/db/Run.kt
+++ app/src/main/java/com/sargis/khlopuzyan/pl_mvvmrunningapp/db/Run.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "running_table")
 data class Run(
-    var image: Bitmap? = null,
-    var timesStamp: Long = 0L,
+    var img: Bitmap? = null,
+    var timestamp: Long = 0L,
     var avgSpeedInKMH: Float = 0f,
     var distanceInMeters: Int = 0,
     var timeInMillis: Long = 0L,
