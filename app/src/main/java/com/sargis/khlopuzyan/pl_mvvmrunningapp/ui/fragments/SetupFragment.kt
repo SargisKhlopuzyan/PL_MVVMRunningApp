90effb3ff19ab79a41b0d6cdcb00d57c9c6a9456
SargisKhlopuzyan1989 <skholpuzyan@shophq.com>
2021-11-11 19:32:19 +0400

    Setting up Navigation - MVVM Running Tracker App - Part 8

--- app/src/main/java/com/sargis/khlopuzyan/pl_mvvmrunningapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/sargis/khlopuzyan/pl_mvvmrunningapp/ui/fragments/SetupFragment.kt
@@ -1,7 +1,20 @@
 package com.sargis.khlopuzyan.pl_mvvmrunningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.sargis.khlopuzyan.pl_mvvmrunningapp.R
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+
+    }
 }
